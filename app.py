fe8a8ee86cff97c75b9f23af8b5847533948e25f
Takashi <45578020+ThatTakashi@users.noreply.github.com>
2026-05-22 14:40:12 +1200

    Update home endpoint function

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return 'API Works!!!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001, host='0.0.0.0')
